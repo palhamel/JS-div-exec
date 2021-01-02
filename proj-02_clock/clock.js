f2fbfbefc1154a2a4f1484d9a7449c600f79d5bd
Pål Hamel <pal.hamel@gmail.com>
2021-01-02 19:39:44 +0100

    cleanup and move js

---
+++ proj-02_clock/clock.js
@@ -0,0 +1,57 @@
+// Analog clock:
+// select class:
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  // seconds to degrees:
+  const seconds = now.getSeconds();
+  const secDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secDegrees}deg)`;
+  // console.log('seconds:', seconds);
+
+  // minutes to degrees:
+  const minutes = now.getMinutes();
+  const minDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+  // console.log('min:', minutes)
+
+  // hours to degrees:
+  const hours = now.getHours();
+  const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  // console.log('hour:', hours)
+}
+// run function:
+setInterval(setDate, 1000);
+// -----------------
+
+// digital clock:
+function currentTime() {
+  const date = new Date(); /* creating object of Date class */
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("digital-clock").innerText =
+    hour + " : " + min + " : " + sec; /* adding time to the div */
+  const time = setTimeout(function () {
+    currentTime();
+  }, 1000); /* setting timer */
+}
+// check 00 digits or add one 0:
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
+
