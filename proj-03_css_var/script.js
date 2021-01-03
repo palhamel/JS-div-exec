595cde46c530f704413e9e6ecc8a17aa753415d3
Pål Hamel <pal.hamel@gmail.com>
2021-01-03 16:11:59 +0100

    clean upp files

--- proj-03_css_var/script.js
+++ proj-03_css_var/script.js
@@ -1,11 +1,14 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
   // console.log(this.data);
-  const suffix = this.dataset.sizing || '';
+  const suffix = this.dataset.sizing || "";
 
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
