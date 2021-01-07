c43db97c1736e33d5f5ca48b728da270efda7103
Pål Hamel <pal.hamel@gmail.com>
2021-01-07 16:21:32 +0100

    setup fetch api

--- proj-06_ajax_typ_ahead/script.js
+++ proj-06_ajax_typ_ahead/script.js
@@ -1,13 +1,17 @@
-const endpoint =
+const apiData =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-console.log("yes im here");
+  console.log("yes im here");
 
-// empty array:
+// empty array - push the json response into this array:
 const cities = [];
 
-fetch(endpoint)
+fetch(apiData)
   .then(res => res.json())
-  .then(json => {
-    console.log(json)
+  .then(data => {
+    cities.push(...data)
   })
+
+console.log('the cities array:', cities)
+
+function findMatches()
